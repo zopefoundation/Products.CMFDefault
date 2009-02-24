481aa5f3a8c37adc87a52b45db7bf3c11adaae07
Charlie Clark <charlie.clark@clark-consulting.eu>
2009-02-24 22:00:50 +0000

    First unittest

--- Products/CMFDefault/browser/tests/test_folder.py
+++ Products/CMFDefault/browser/tests/test_folder.py
@@ -10,24 +10,51 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""CMFDefault browser tests.
-
-$Id: tests.py 92781 2008-11-04 17:43:00Z yuppie $
+"""Test Products.CMFDefault.browser.new_folder BrowserView tests
+$Id$
 """
 
 import unittest
-from Testing import ZopeTestCase
-from zope.testing import doctest
 
-from Products.CMFDefault.testing import FunctionalLayer
+from AccessControl.SecurityManagement import newSecurityManager
 
+from zope.component import getSiteManager
+from zope.publisher.browser import TestRequest
+from zope.publisher.interfaces.browser import IBrowserPublisher
+
+from Products.CMFCore.PortalFolder import PortalFolder
+from Products.CMFCore.tests.base.dummy import DummySite
+from Products.CMFCore.tests.base.dummy import DummyTool
+from Products.CMFCore.tests.base.dummy import DummyUserFolder
+from Products.CMFCore.interfaces import IPropertiesTool
+
+from Products.CMFDefault.browser.new_folder import ContentsView
+
+class FolderBrowserViewTests(unittest.TestCase):
+
+    def setUp(self):
+        """Setup a site"""
+        # maybe there is a base class for this?
+        self.site = site = DummySite('site')
+        # acl_users = site._setObject('acl_users', DummyUserFolder())
+        # newSecurityManager(None, acl_users.all_powerful_Oz)
+        self.sm = getSiteManager()
+        mtool = site._setObject('portal_membership', DummyTool())
+        ptool = site._setObject('portal_properties', DummyTool())
+        self.sm.registerUtility(ptool, IPropertiesTool)
+        ttool = site._setObject('portal_types', DummyTool())
+        utool = site._setObject('portal_url', DummyTool())
+        folder = PortalFolder('test_folder')
+        self.folder = site._setObject('test_folder', folder)
+    
+    def test_view(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.failUnless(IBrowserPublisher.providedBy(view))
+        
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(doctest.DocFileSuite('folder_utest.txt',
-                                    optionflags=doctest.NORMALIZE_WHITESPACE))
+    suite.addTest(unittest.makeSuite(FolderBrowserViewTests))
     return suite
-
-if __name__ == '__main__':
-    from Products.CMFCore.testing import run
-    run(test_suite())
+    
+# bin/test -s ~/CMF-Sandbox/cmf-trunk/src/Products.CMFDefault/Products/CMFDefault/browser
