85deb27b9506604e3ace49ca7e517f56be25fcb7
Charlie Clark <charlie.clark@clark-consulting.eu>
2009-02-25 12:04:45 +0000

    Added the rest of the basic tests.

--- Products/CMFDefault/browser/tests/test_folder.py
+++ Products/CMFDefault/browser/tests/test_folder.py
@@ -25,7 +25,7 @@ from zope.publisher.interfaces.browser import IBrowserPublisher
 from Products.CMFCore.PortalFolder import PortalFolder
 from Products.CMFCore.tests.base.dummy import DummySite
 from Products.CMFCore.tests.base.dummy import DummyTool
-from Products.CMFCore.tests.base.dummy import DummyUserFolder
+from Products.CMFCore.tests.base.dummy import DummyUserFolder, DummyContent
 from Products.CMFCore.interfaces import IPropertiesTool
 
 from Products.CMFDefault.browser.new_folder import ContentsView
@@ -36,8 +36,6 @@ class FolderBrowserViewTests(unittest.TestCase):
         """Setup a site"""
         # maybe there is a base class for this?
         self.site = site = DummySite('site')
-        # acl_users = site._setObject('acl_users', DummyUserFolder())
-        # newSecurityManager(None, acl_users.all_powerful_Oz)
         self.sm = getSiteManager()
         mtool = site._setObject('portal_membership', DummyTool())
         ptool = site._setObject('portal_properties', DummyTool())
@@ -51,6 +49,34 @@ class FolderBrowserViewTests(unittest.TestCase):
         view = ContentsView(self.folder, TestRequest())
         self.failUnless(IBrowserPublisher.providedBy(view))
         
+    def test_up_info(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.assertEquals({'url':u'', 'id':u'Root', 'icon':u''}, view.up_info())
+        
+    def test_layout_fields(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.assertEquals(view.layout_fields(), [])
+    
+    def test_is_orderable(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.failIf(view.is_orderable())
+        
+    def test_sort_can_be_changed(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.failIf(view.can_sort_be_changed())
+    
+    def test_has_subobjects(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.failIf(view.has_subobjects())
+        
+    def test_check_clipboard_data(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.failIf(view.check_clipboard_data())
+    
+    def test_check_validator(self):
+        view = ContentsView(self.folder, TestRequest())
+        self.assertEquals(view.validate_items(), [u'Please select one or more items first.'])
+        self.assertEquals(view.validate_items(data={'foo':True}), [])
 
 def test_suite():
     suite = unittest.TestSuite()
