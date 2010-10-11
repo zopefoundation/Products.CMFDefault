eecf7e94c34a323d4f77f1fed0710d6d75997729
Charlie Clark <charlie.clark@clark-consulting.eu>
2010-10-11 18:27:55 +0000

    Duplicated copyright notice removed.

--- Products/CMFDefault/browser/search/search.py
+++ Products/CMFDefault/browser/search/search.py
@@ -10,18 +10,6 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-##############################################################################
-#
-# Copyright (c) 2010 Zope Foundation and Contributors.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
 """Search views"""
 import datetime
 
