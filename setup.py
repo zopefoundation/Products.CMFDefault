344f8eac816e2cd9c3e03d56844429bfe080eb06
Jens Vagelpohl <jens@netz.ooo>
2021-02-04 12:48:21 +0100

    - clean up buildout and setup configurations

--- setup.py
+++ setup.py
@@ -1,48 +1,56 @@
 import os
-from setuptools import setup
+
 from setuptools import find_packages
+from setuptools import setup
+
 
 NAME = 'CMFDefault'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 def _package_doc(name):
     f = open(os.path.join(here, name))
     return f.read()
 
+
 _boundary = '\n' + ('-' * 60) + '\n\n'
-README = ( _package_doc('README.rst')
-         + _boundary
-         + _package_doc('CHANGES.rst')
-         + _boundary
-         + "Download\n========"
-         )
+README = _boundary.join([
+    _package_doc('README.rst'),
+     _package_doc('CHANGES.rst'),
+])
 
 setup(name='Products.%s' % NAME,
       version='2.3.0.dev0',
       description='Default product for the Zope Content Management Framework',
       long_description=README,
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Plone',
+        'Framework :: Zope :: 2',
+        'Framework :: Zope2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
         ],
       keywords='web application server zope zope2 cmf',
-      author="Zope Foundation and Contributors",
-      author_email="zope-cmf@zope.org",
-      url="http://pypi.python.org/pypi/Products.CMFDefault",
-      license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
+      author='Zope Foundation and Contributors',
+      author_email='zope-cmf@zope.org',
+      url='https://github.com/zopefoundation/Products.CMFDefault',
+      project_urls={
+          'Issue Tracker': ('https://github.com/zopefoundation/'
+                            'Products.CMFDefault/issues'),
+          'Sources': 'https://github.com/zopefoundation/Products.CMFDefault',
+      },
+      license='ZPL 2.1',
       packages=find_packages(),
       include_package_data=True,
       namespace_packages=['Products'],
       zip_safe=False,
-      setup_requires=['eggtestinfo',
-                     ],
+      python_requires='>=2.7, <3.0',
       install_requires=[
           'setuptools',
           'Zope2 >= 2.13.12',
@@ -58,17 +66,15 @@ setup(name='Products.%s' % NAME,
           'zope.testing >= 3.7.0',
           'Products.DCWorkflow < 2.4',
           ],
-      extras_require={ 'test': ['Products.DCWorkflow < 2.4'],
-                       'docs': ['Sphinx', 
-                                'repoze.sphinx.autointerface', 
-                                'pkginfo'],
-                       'locales': ['zope.app.locales'],},
+      extras_require={
+          'test': ['Products.DCWorkflow < 2.4'],
+          'docs': ['Sphinx', 'repoze.sphinx.autointerface', 'pkginfo'],
+          'locales': ['zope.app.locales'],
+          },
       test_loader='zope.testing.testrunner.eggsupport:SkipLayers',
       test_suite='Products.%s' % NAME,
       entry_points="""
       [zope2.initialize]
       Products.%s = Products.%s:initialize
-      [distutils.commands]
-      ftest = zope.testing.testrunner.eggsupport:ftest
       """ % (NAME, NAME),
       )
